11971925ad75dcb52b6b6b9c135777f44816bdb0
Marcel Ruegenberg <m.rue@picodesign.de>
2017-02-20 10:05:26 +0100

    Use programmable renderer in oF 0.9

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -3,7 +3,13 @@
 
 //--------------------------------------------------------------
 int main(){
-    // set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-    ofSetupOpenGL(1024, 768, OF_WINDOW);
-    ofRunApp(new testApp()); // start the app
+    // ofGLFWWindowSettings settings;
+    ofGLWindowSettings settings;
+    settings.width = 1280;
+    settings.height = 960;
+    settings.windowMode = OF_WINDOW;
+    settings.setGLVersion(4,5); // use this for native buffer objects etc
+    ofCreateWindow(settings);
+    
+    ofRunApp(new testApp());
 }
